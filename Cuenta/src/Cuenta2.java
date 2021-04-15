5a079f1c73302bcba4a14cac08fcefd70d1a8dbf
Juankuus <juanc.mormar.1@educa.jcyl.es>
2021-04-15 19:06:41 +0200

    dsdasdas

--- Cuenta/src/Cuenta2.java
+++ Cuenta/src/Cuenta2.java
@@ -1,23 +1,24 @@
 
 public class Cuenta2 {
-	
+
 	int conta;
 	int total;
-	
+
 	int valor;
-	
+
 	int interes;
-	
+
 	int raul;
-	
+
 	int raul2;
-	
+
 	int cambio;
-	
+
 	int raul4;
-	
-	
+
 	int daw;
 
 	int pablo;
+
+	int jcok;
 }
